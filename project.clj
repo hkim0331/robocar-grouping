17511ded7589c23bb08ab69447102d2bd5f78a33
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-12-12 22:50:23 +0900

    release 0.2.3 delete

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gr "0.2.2"
+(defproject gr "0.2.3"
   :description "robocar 2021 make groups app"
   :url "https://gr.melt.kyutech.ac.jp"
   :min-lein-version "2.0.0"
--- src/gr/view/page.clj
+++ src/gr/view/page.clj
@@ -10,7 +10,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [taoensso.timbre :as timbre :refer [debug]]))
 
-(def version "0.2.2")
+(def version "0.2.3")
 
 (defn page [& contents]
   [::response/ok
