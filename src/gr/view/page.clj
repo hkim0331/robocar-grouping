9533d126f65693ba3372f1875512e139c7cf7998
Hiroshi Kimura <hiroshi.kimura.0331@gmail.com>
2021-12-12 12:35:57 +0900

    project started

---
+++ src/gr/view/page.clj
@@ -0,0 +1,52 @@
+(ns gr.view.page
+  (:require
+   [ataraxy.response :as response]
+   [clojure.string :as str]
+   [hiccup.page :refer [html5]]
+   [hiccup.form :refer [form-to text-field password-field submit-button
+                        label text-area file-upload hidden-field]]
+   [hiccup.util :refer [escape-html]]
+   [ring.util.anti-forgery :refer [anti-forgery-field]]
+   [taoensso.timbre :as timbre :refer [debug]]))
+
+(def version "0.7.9")
+
+(defn page [& contents]
+  [::response/ok
+   (html5
+    [:head
+     [:meta {:charset "utf-8"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]]
+    [:link
+     {:rel "stylesheet"
+      :href "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+      :integrity "sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+      :crossorigin "anonymous"}]
+    [:link
+     {:rel "stylesheet"
+      :type "text/css"
+      :href "/css/styles.css"}]
+    [:script {:type "text/javascript"}
+     "function ok() {return window.confirm('OK?');}"]
+    [:title "gr"]
+    [:body
+     [:div {:class "container"}
+      contents
+      [:p]
+      [:p [:a {:href "/logout" :class "btn btn-warning btn-sm"} "logout"]]
+      [:hr]
+      "hkimura, " version "."]])])
+
+
+(defn login-page []
+  (page
+   [:h2 "gr: Login"]
+   [:p "r99.melt と同じやつで。"
+    [:a {:href "/"} "注意事項"]]
+   (form-to
+    [:post "/login"]
+    (anti-forgery-field)
+    (text-field {:placeholder "ニックネーム"} "nick")
+    (password-field {:placeholder "パスワード"} "password")
+    (submit-button "login"))))
+
