f7e20dd36f3b80163ff1654fa506fbb19932279b
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2021-12-13 09:15:51 +0900

    changed top page

--- src/gr/view/page.clj
+++ src/gr/view/page.clj
@@ -10,7 +10,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [taoensso.timbre :as timbre :refer [debug]]))
 
-(def version "0.3.0")
+(def version "0.3.1")
 
 (defn page [& contents]
   [::response/ok
