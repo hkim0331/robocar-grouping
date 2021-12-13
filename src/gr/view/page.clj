0fedb6dff6a81520cb7508755c30edf9f8a19874
Hiroshi Kimura(m3) <hiroshi.kimura.0331@gmail.com>
2021-12-13 10:43:38 +0900

    release 0.4.0

--- src/gr/view/page.clj
+++ src/gr/view/page.clj
@@ -10,7 +10,7 @@
    [ring.util.anti-forgery :refer [anti-forgery-field]]
    [taoensso.timbre :as timbre :refer [debug]]))
 
-(def version "0.3.2")
+(def version "0.4.0")
 
 (defn page [& contents]
   [::response/ok
